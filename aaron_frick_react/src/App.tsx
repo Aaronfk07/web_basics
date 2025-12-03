23c2dcc91b97eec97a666bf806840dd036012a57
Aaron Frick <ghost.aaron@gmx.at>
2025-12-03 10:42:33 +0100

    React_Task_2 Done

--- aaron_frick_react/src/App.tsx
+++ aaron_frick_react/src/App.tsx
@@ -1,39 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import List from './components/List'
+import Card from './components/card';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <h2 style={{ marginTop: '2rem' }}>Demo List</h2>
-      <p style={{ marginBottom: '0.5rem' }}>Hover over items and click to see a popup.</p>
-      <List />
-    </>
-  )
+  <div>
+    <h1>Hello</h1>
+<Card/>
+  </div>
+);
 }
 
-export default App
