66bae3ff82b58560d8c9ca13aaf3abaeaa396f21
Aaron Frick <ghost.aaron@gmx.at>
2025-12-03 11:04:58 +0100

    React_Task_3 done

--- aaron_frick_react/src/App.tsx
+++ aaron_frick_react/src/App.tsx
@@ -1,11 +1,14 @@
-import './App.css'
 import Card from './components/card';
 
 export default function App() {
   return (
   <div>
     <h1>Hello</h1>
-<Card/>
+<div className='grid grid-cols-2 w-80 gap-4'>
+<Card title='aaron' description='A description about aaron' />
+<Card title='susi' description='A description about susi' />
+
+</div>
   </div>
 );
 }
--- aaron_frick_react/src/components/card.tsx
+++ aaron_frick_react/src/components/card.tsx
@@ -1,3 +1,14 @@
-export default function Card() {
-return <div>Card Component</div>;
+type Props = {
+    title: string;
+    description: string;
+}
+
+export default function card({title, description}: Props) {
+  return (
+    <div className="border bg-green-400">
+<h2>{title}</h2>
+<p>{description}</p>
+
+    </div>
+  )
 }
